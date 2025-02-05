d77291ac2c0268427885c5a643ebdeb19089e266
MrBirender <birenderyadav0918@gmail.com>
2025-02-05 13:06:23 +0530

    error resolved

--- src/features/counterSlice.ts
+++ src/features/counterSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice} from "@reduxjs/toolkit";
 
 const getInitialCount = (): number => {
   const savedCount = localStorage.getItem("counter");
